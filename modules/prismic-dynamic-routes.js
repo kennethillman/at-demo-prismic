5188ef45480120c00b908f7eb5a32cea2603cd1a
Kenneth Illman <kenneth.illman@gmail.com>
2020-03-03 11:18:22 +0530

    clean up

--- modules/prismic-dynamic-routes.js
+++ modules/prismic-dynamic-routes.js
@@ -5,13 +5,9 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 
-
-
 export default async function asyncModule (context, error, req) {
 
 	try{
-
-		console.log('- - - > module -> prismic-dynamic-routes')
 
 		// GET
 	    const api = await Prismic.getApi(process.env.PRISMIC_API, {req})
@@ -23,12 +19,8 @@ export default async function asyncModule (context, error, req) {
 	    const prismicRoutesRoutes = prismicRoutesRes.results.map((cont) => {
 		    return '/content/'+cont.uid
 		})
-
-	    // console.log('prismicRoutesRoutes -> ' , prismicRoutesRoutes)
-
 	    
 	    fse.outputJSON('./_data/prismic-routes.json', prismicRoutesRoutes, { spaces: 4 })
-	
 
     } catch (e) {
       console.log(e)
